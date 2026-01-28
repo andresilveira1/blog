8a88e49a993e34a61674d4a995cb315a93ab7f71
andresilveira1 <andre.silveira1silva@gmail.com>
2026-01-27 23:02:02 -0300

    chore: add contentlayer config.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from "next-contentlayer";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
