5c556c63d25ed7a7518475fe569124e67a70f09d
andresilveira1 <andre.silveira1silva@gmail.com>
2026-02-07 15:07:01 -0300

    feat: add search function and a visual element to inform if there is a post.

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -4,67 +4,67 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 
 export const Search = () => {
-  const router = useRouter();
-  const query = (router.query.q as string) ?? "";
+	const router = useRouter();
+	const query = (router.query.q as string) ?? "";
 
-  const handleSearch = useCallback(
-    (event: React.FormEvent) => {
-      event.preventDefault();
+	const handleSearch = useCallback(
+		(event: React.FormEvent) => {
+			event.preventDefault();
 
-      if (query.trim()) {
-        router.push(`/blog?q=${encodeURIComponent(query)}}`);
-      }
-    },
-    [query, router],
-  );
+			if (query.trim()) {
+				router.push(`/blog?q=${encodeURIComponent(query)}}`);
+			}
+		},
+		[query, router],
+	);
 
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuery = event.target.value;
+	const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newQuery = event.target.value;
 
-    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
-      shallow: true,
-      scroll: false,
-    });
-  };
+		router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
+			shallow: true,
+			scroll: false,
+		});
+	};
 
-  const clearInputSearch = () => {
-    router.push("/blog", undefined, {
-      shallow: true,
-      scroll: false,
-    });
-  };
+	const clearInputSearch = () => {
+		router.push("/blog", undefined, {
+			shallow: true,
+			scroll: false,
+		});
+	};
 
-  return (
-    <form
-      onSubmit={handleSearch}
-      className="flex items-center border px-4 py-3 rounded-md w-[358px] md:w-60 group border-gray-400 transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300"
-    >
-      <label htmlFor="search">
-        <SearchIcon
-          className={cn(
-            "mr-3 transition-colors duration-200 group-focus-within:text-blue-200",
-            query ? "text-blue-200" : "text-gray-300",
-          )}
-          size={16}
-        />
-      </label>
+	return (
+		<form
+			onSubmit={handleSearch}
+			className="flex items-center border px-4 py-3 rounded-md w-[358px] md:w-60 group border-gray-400 transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300"
+		>
+			<label htmlFor="search">
+				<SearchIcon
+					className={cn(
+						"mr-3 transition-colors duration-200 group-focus-within:text-blue-200",
+						query ? "text-blue-200" : "text-gray-300",
+					)}
+					size={16}
+				/>
+			</label>
 
-      <input
-        type="text"
-        id="search"
-        value={query}
-        onChange={handleQueryChange}
-        placeholder="Buscar"
-        className="bg-transparent outline-none w-fit text-gray-100 placeholder:text-gray-300 text-body-sm"
-      />
+			<input
+				type="text"
+				id="search"
+				value={query}
+				onChange={handleQueryChange}
+				placeholder="Buscar"
+				className="bg-transparent outline-none w-fit text-gray-100 placeholder:text-gray-300 text-body-sm"
+			/>
 
-      {query && (
-        <CircleX
-          className="text-gray-300"
-          size={16}
-          onClick={clearInputSearch}
-        />
-      )}
-    </form>
-  );
+			{query && (
+				<CircleX
+					className="text-gray-300"
+					size={16}
+					onClick={clearInputSearch}
+				/>
+			)}
+		</form>
+	);
 };
--- src/templates/blog/index.tsx
+++ src/templates/blog/index.tsx
@@ -3,6 +3,7 @@ import { allPosts } from "contentlayer/generated";
 
 import { Search } from "@/components/search";
 import { PostCard } from "./components/post-card";
+import { Inbox } from "lucide-react";
 
 export function BlogList() {
 	const router = useRouter();
@@ -12,7 +13,13 @@ export function BlogList() {
 		? `Resultados de busca para "${query}"`
 		: "Dicas e estratégias para impulsionar seu negócio";
 
-	const posts = allPosts;
+	const posts = query
+		? allPosts.filter((post) =>
+				post.title.toLowerCase()?.includes(query.toLowerCase()),
+			)
+		: allPosts;
+
+	const hasPosts = posts.length > 0;
 
 	return (
 		<div className="flex flex-col py-24 flex-grow h-full">
@@ -31,21 +38,32 @@ export function BlogList() {
 				</div>
 			</header>
 
-			<div className="container flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-				{posts.map((post) => {
-					return (
-						<PostCard
-							key={post._id}
-							title={post.title}
-							description={post.description}
-							date={new Date(post.date).toLocaleDateString("pt-BR")}
-							image={post.image}
-							slug={post.slug}
-							author={{ name: post.author.name, avatar: post.author.avatar }}
-						/>
-					);
-				})}
-			</div>
+			{hasPosts && (
+				<div className="container flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+					{posts.map((post) => {
+						return (
+							<PostCard
+								key={post._id}
+								title={post.title}
+								description={post.description}
+								date={new Date(post.date).toLocaleDateString("pt-BR")}
+								image={post.image}
+								slug={post.slug}
+								author={{ name: post.author.name, avatar: post.author.avatar }}
+							/>
+						);
+					})}
+				</div>
+			)}
+
+			{!hasPosts && (
+				<div className="container px-8">
+					<div className="flex flex-col items-center justify-center gap-8 border-dashed border-2 border-gray-300 p-8 md:p-12 rounded-lg">
+						<Inbox className="h-12 w-12 text-cyan-100" />
+						<p className="text-gray-100 text-center">Nenhum post encontrado.</p>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
