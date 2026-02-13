7136e7513298226b1618c24bad5a7eeedef3c4ed
andresilveira1 <andre.silveira1silva@gmail.com>
2026-02-13 00:22:42 -0300

    refactor: remove call to action component from landing page and add in the global layout.

--- src/templates/landing-page/index.tsx
+++ src/templates/landing-page/index.tsx
@@ -1,19 +1,17 @@
 import {
-  HeroSection,
-  FeatureSection,
-  SupportSection,
-  CustomerStorySection,
-  CallToActionSection,
+	HeroSection,
+	FeatureSection,
+	SupportSection,
+	CustomerStorySection,
 } from "@/templates/landing-page/sections";
 
 export const LandingPage = () => {
-  return (
-    <article className="flex flex-col">
-      <HeroSection />
-      <FeatureSection />
-      <SupportSection />
-      <CustomerStorySection />
-      <CallToActionSection />
-    </article>
-  );
+	return (
+		<article className="flex flex-col">
+			<HeroSection />
+			<FeatureSection />
+			<SupportSection />
+			<CustomerStorySection />
+		</article>
+	);
 };
