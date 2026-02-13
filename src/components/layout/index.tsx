2149126f1854d7323ae735b4ec1ff27f01edc4e9
andresilveira1 <andre.silveira1silva@gmail.com>
2026-02-13 00:18:23 -0300

    refactor: move call to action component to global layout.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,31 +2,33 @@ import { Inter, PT_Sans_Caption } from "next/font/google";
 
 import { Footer } from "./footer";
 import { Header } from "./header";
+import { CallToActionSection } from "./call-to-action-section";
 
 type LayoutProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500"],
-  variable: "--font-inter",
+	subsets: ["latin"],
+	weight: ["400", "500"],
+	variable: "--font-inter",
 });
 
 const ptSansCaption = PT_Sans_Caption({
-  subsets: ["latin"],
-  weight: "700",
-  variable: "--font-sans",
+	subsets: ["latin"],
+	weight: "700",
+	variable: "--font-sans",
 });
 
 export const Layout = ({ children }: LayoutProps) => {
-  return (
-    <div
-      className={`${inter.className} ${ptSansCaption.className} font-inter relative flex min-h-screen flex-col bg-gray-700`}
-    >
-      <Header />
-      <main className="flex flex-1 flex-col">{children}</main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div
+			className={`${inter.className} ${ptSansCaption.className} font-inter relative flex min-h-screen flex-col bg-gray-700`}
+		>
+			<Header />
+			<main className="flex flex-1 flex-col">{children}</main>
+			<CallToActionSection />
+			<Footer />
+		</div>
+	);
 };
