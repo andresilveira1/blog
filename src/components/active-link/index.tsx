9bf74e5fbfac7162ac70d9edffcbd690a4fc6f69
andresilveira1 <andre.silveira1silva@gmail.com>
2026-02-23 21:17:11 -0300

    refactor: migrate Page Routes to App Router and change component to Client Side.

--- src/components/active-link/index.tsx
+++ src/components/active-link/index.tsx
@@ -1,21 +1,26 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import Link, { type LinkProps } from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 type ActiveLink = {
 	children: React.ReactNode;
 } & LinkProps;
 
 export const ActiveLink = ({ children, href, ...props }: ActiveLink) => {
-	const router = useRouter();
-	const isCurrentPath = router.asPath === href || router.asPath === props.as;
+	const linkPath = (typeof href === "string" ? href : href.pathname) ?? "";
+	const pathname = usePathname();
+	const isActive =
+		pathname === linkPath || pathname?.startsWith(`${linkPath}/`);
 
 	return (
 		<Link
+			{...props}
 			href={href}
 			className={cn(
 				"text-action-sm transition-colors hover:text-blue-200",
-				isCurrentPath ? "text-blue-200" : "text-gray-100",
+				isActive ? "text-blue-200" : "text-gray-100",
 			)}
 		>
 			{children}
